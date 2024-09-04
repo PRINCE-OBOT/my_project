9a992c3307752ea265c84d6a5d61e2487d1516f5
PRINCE-OBOT <princessamuel45@gmail.com>
2024-09-04 17:09:47 +0100

    Add playGame function run the complete game and determine who wins

--- script.js
+++ script.js
@@ -40,28 +40,28 @@ function playRound(humanChoice, computerChoice){
         console.log(`Your choice: ${humanChoice} \nComputer choice: ${computerChoice}\n${computerChoice} beat ${humanChoice}`)
     }
 }
-    function playGame(){
-        let start = confirm('Play rock, paper, scissors game')
-            if(start == false){
-                console.log('cancelled')
-                return 
-            }
-           
-            for(i=0; i<5; i++){
-                let humanChoice = getHumanChoice()
-                let computerChoice = getComputerChoice()
-                playRound(humanChoice, computerChoice)
-                console.log(`Your score: ${humanScore} \nComputer Score: ${computerScore}`)
-            }
-            if(humanScore > computerScore){
-                console.log('You win!')
-            }
-            else if (humanScore < computerScore){
-                console.log('You lose!')
-            }
-            else{
-                ('Draw')
-            }
-            }
-            playGame()
-    
+   
+function playGame(){
+    let start = confirm('Play rock, paper, scissors game')
+        if(start == false){
+            console.log('cancelled')
+            return 
+        }
+       
+        for(i=0; i<5; i++){
+            let humanChoice = getHumanChoice()
+            let computerChoice = getComputerChoice()
+            playRound(humanChoice, computerChoice)
+            console.log(`Your score: ${humanScore} \nComputer Score: ${computerScore}`)
+        }
+        if(humanScore > computerScore){
+            console.log('You win!')
+        }
+        else if (humanScore < computerScore){
+            console.log('You lose!')
+        }
+        else{
+            ('Draw')
+        }
+        }
+        playGame()
