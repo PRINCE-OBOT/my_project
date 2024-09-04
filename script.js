ed6ef9ca201e5059919e5e1aec2d26b18984261e
PRINCE-OBOT <princessamuel45@gmail.com>
2024-09-04 17:07:49 +0100

    Add playRound function enable the program to user input, computer choice and score increement

--- script.js
+++ script.js
@@ -7,16 +7,61 @@ function getComputerChoice(){
     return choices[getRandomIndex]
 }
 
+
+
 function getHumanChoice(){
-    let humanChoice = prompt('Input your choice(Rock, paper or scissor)').toLowerCase()
+    let humanChoice = prompt('Input your choice(Rock, paper or scissor)')
+    humanChoice = humanChoice.toLowerCase()
     if(humanChoice === 'rock' || humanChoice ==='scissors' || humanChoice === 'paper'){
         return humanChoice
     } 
     else{
         alert('Invalid input')
+        return getHumanChoice()
     }
 }
 
-humanScore = 0
-computerScore = 0
+let humanScore = 0
+let computerScore = 0
 
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        console.log(`Your choice: ${humanChoice}\nComputer choice: ${computerChoice}\nEqual`)
+    }
+    else if((humanChoice ==='rock' && computerChoice ==='scissors') ||
+            (humanChoice ==='paper' && computerChoice ==='rock') ||
+            (humanChoice ==='scissors' && computerChoice === 'paper')){
+                humanScore++
+                console.log(`Your choice: ${humanChoice} \nComputer choice:${computerChoice}\n${humanChoice} beat ${computerChoice}`)
+                
+            }
+    else{
+        computerScore++
+        console.log(`Your choice: ${humanChoice} \nComputer choice: ${computerChoice}\n${computerChoice} beat ${humanChoice}`)
+    }
+}
+    function playGame(){
+        let start = confirm('Play rock, paper, scissors game')
+            if(start == false){
+                console.log('cancelled')
+                return 
+            }
+           
+            for(i=0; i<5; i++){
+                let humanChoice = getHumanChoice()
+                let computerChoice = getComputerChoice()
+                playRound(humanChoice, computerChoice)
+                console.log(`Your score: ${humanScore} \nComputer Score: ${computerScore}`)
+            }
+            if(humanScore > computerScore){
+                console.log('You win!')
+            }
+            else if (humanScore < computerScore){
+                console.log('You lose!')
+            }
+            else{
+                ('Draw')
+            }
+            }
+            playGame()
+    
