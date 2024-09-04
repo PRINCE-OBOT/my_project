82827157714dc1f957f0af8b48ad9dd860ab8a8d
PRINCE-OBOT <princessamuel45@gmail.com>
2024-09-04 14:42:42 +0100

    Add human choice will return one of the valid choices depending on what user inputs

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function getComputerChoice(){
-const computerChoice = ["Rock","Paper","Scissors"]
-const getRandomIndex = Math.floor(Math.random()*computerChoice.length)
-return computerChoice[getRandomIndex]
-}
-console.log(getComputerChoice())
 
+
+const choices = ["rock","paper","scissors"]
+
+function getComputerChoice(){
+    let getRandomIndex = Math.floor(Math.random()*choices.length)
+    return choices[getRandomIndex]
+}
+
+function getHumanChoice(){
+    let humanChoice = prompt('Input your choice(Rock, paper or scissor)').toLowerCase()
+    if(humanChoice === 'rock' || humanChoice ==='scissors' || humanChoice === 'paper'){
+        return humanChoice
+    } 
+    else{
+        alert('Invalid input')
+    }
+}
+console.log(getHumanChoice())
