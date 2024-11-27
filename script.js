1d5cdaf5e713b89422a1b7d420729e76b42cf3d7
PRINCE-OBOT <princessamuel45@gmail.com>
2024-11-27 11:19:47 +0100

    Update Rock, Paper, Scissors
    Enhance display of result
    and avoid error when user
    end game before it complete
    tournament.

--- script.js
+++ script.js
@@ -1,67 +1,94 @@
+let humanScore = 0;
+let computerScore = 0;
 
+if(confirm(`Do you want to play (Rock, Paper, Scissors)
 
-const choices = ["rock","paper","scissors"]
+Click "OK" to start game`)){
+let i = 1;
+while(i<=5){
+  let process = true
+ function getComputerChoice(){
+const choices = ["rock", "paper", "scissors"]
+const randomIndex = Math.floor(Math.random()*choices.length)
+return choices[randomIndex]
+ }
+ 
+ function getHumanChoice(){
+ while(true){
+const enteredChoice = prompt("Enter your choice")
 
-function getComputerChoice(){
-    let getRandomIndex = Math.floor(Math.random()*choices.length)
-    return choices[getRandomIndex]
-}
-
-
-
-function getHumanChoice(){
-    let humanChoice = prompt('Input your choice(Rock, paper or scissor)')
-    humanChoice = humanChoice.toLowerCase()
-    if(humanChoice === 'rock' || humanChoice ==='scissors' || humanChoice === 'paper'){
-        return humanChoice
-    } 
-    else{
-        alert('Invalid input')
-        return getHumanChoice()
-    }
-}
-
-let humanScore = 0
-let computerScore = 0
-
-function playRound(humanChoice, computerChoice){
-    if(humanChoice === computerChoice){
-        console.log(`Your choice: ${humanChoice}\nComputer choice: ${computerChoice}\nEqual`)
-    }
-    else if((humanChoice ==='rock' && computerChoice ==='scissors') ||
-            (humanChoice ==='paper' && computerChoice ==='rock') ||
-            (humanChoice ==='scissors' && computerChoice === 'paper')){
-                humanScore++
-                console.log(`Your choice: ${humanChoice} \nComputer choice:${computerChoice}\n${humanChoice} beat ${computerChoice}`)
-                
-            }
-    else{
-        computerScore++
-        console.log(`Your choice: ${humanChoice} \nComputer choice: ${computerChoice}\n${computerChoice} beat ${humanChoice}`)
-    }
-}
+if(enteredChoice){
+const userChoice = enteredChoice.toLowerCase()
    
-function playGame(){
-    let start = confirm('Play rock, paper, scissors game')
-        if(start == false){
-            console.log('cancelled')
-            return 
-        }
-       
-        for(i=0; i<5; i++){
-            let humanChoice = getHumanChoice()
-            let computerChoice = getComputerChoice()
-            playRound(humanChoice, computerChoice)
-            console.log(`Your score: ${humanScore} \nComputer Score: ${computerScore}`)
-        }
-        if(humanScore > computerScore){
-            console.log('You win!')
-        }
-        else if (humanScore < computerScore){
-            console.log('You lose!')
-        }
-        else{
-            ('Draw')
-        }
-        }
-        playGame()
+  if(userChoice === "rock" || userChoice === "scissors" || userChoice === "paper"){return userChoice;}
+  else{alert('Invalid input. Try again')}}
+else if(enteredChoice === ""){alert(`No input. Try again`)}
+else if(!enteredChoice){
+  let checkConfirm = confirm(`Do you want to end the game? Click "OK" to end`)
+  if(checkConfirm){process = false;
+    return null
+  }
+   }
+  }
+ }
+ function playRound(humanChoice, computerChoice){
+   if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper"){
+     humanScore++
+     console.log(`
+Human Choice : ${humanChoice}
+Computer Choice : ${computerChoice}
+You win! ${humanChoice} beat ${computerChoice}
+Human Score: ${humanScore}
+Computer Score : ${computerScore}
+`)
+   }
+   else if(computerChoice === "rock" && humanChoice === "scissors" || computerChoice === "paper" && humanChoice === "rock" || computerChoice === "scissors" && humanChoice === "paper"){
+     computerScore++
+     console.log(`
+Human Choice : ${humanChoice}
+Computer Choice : ${computerChoice}
+You lose! ${computerChoice} beat ${humanChoice}
+Human Score: ${humanScore}
+Computer Score : ${computerScore}
+`)
+   }
+   else if(humanChoice === computerChoice){
+     console.log(`
+Human Choice : ${humanChoice}
+Computer Choice : ${computerChoice}
+- Draw -
+Human Score: ${humanScore}
+Computer Score : ${computerScore}
+`)
+   }
+ }
+ let computerSelection = getComputerChoice();
+ 
+ let humanSelection = getHumanChoice()
+ 
+ 
+ playRound(humanSelection, computerSelection)
+
+ if(!process && i > 1){
+if(humanScore > computerScore){console.log("Human Win - Incomplete tournament")}
+else if(humanScore < computerScore){
+  console.log("Computer Win - Incomplete tournament")}
+else if(humanScore === computerScore){
+  console.log("Draw Game - Incomplete tournament")}
+  break;
+ }
+ else if(!process){
+  console.log("Game cancelled")
+  break;
+ }
+ else if(i === 5){
+   if(humanScore > computerScore){
+  console.log("Human Win")}
+else if(humanScore < computerScore){
+  console.log("Computer Win")}
+else if(humanScore === computerScore){
+  console.log("Draw Game")}
+  }
+ i++
+ }
+}
