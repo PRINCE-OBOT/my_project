b35f699b3aa6d07cf5a822b0dd69541ab34459ec
PRINCE-OBOT <princessamuel45@gmail.com>
2024-11-27 13:30:49 +0100

    Add queryselectorAll and innerHTML
    Apply querySelectorAll in button
    for efficiently returning
    user choice.
    
    innerHTML, with break tag for
    setting specific results and score
    on separate line

--- script.js
+++ script.js
@@ -1,65 +1,54 @@
-let humanScore = 0;
-let computerScore = 0;
-let ome = 2
-if(confirm(`Do you want to play (Rock, Paper, Scissors)
+const buttons = document.querySelectorAll(".btn")
+const displayResult = document.querySelector(".displayResult")
 
-Click "OK" to start game`)){
-let i = 1;
-while(i<=5){
-  let process = true
- function getComputerChoice(){
+ let humanScore = 0;
+let computerScore = 0;
+
+buttons.forEach((btn)=>{
+  btn.addEventListener("click", (e)=>{
+    function getComputerChoice(){
 const choices = ["rock", "paper", "scissors"]
 const randomIndex = Math.floor(Math.random()*choices.length)
 return choices[randomIndex]
  }
  
  function getHumanChoice(){
- while(true){
-const enteredChoice = prompt("Enter your choice")
-
-if(enteredChoice){
-const userChoice = enteredChoice.toLowerCase()
+   let enterChoice = e.target.textContent
    
-  if(userChoice === "rock" || userChoice === "scissors" || userChoice === "paper"){return userChoice;}
-  else{alert('Invalid input. Try again')}}
-else if(enteredChoice === ""){alert(`No input. Try again`)}
-else if(!enteredChoice){
-  let checkConfirm = confirm(`Do you want to end the game? Click "OK" to end`)
-  if(checkConfirm){process = false;
-    return null
-  }
-   }
-  }
- }
- function playRound(humanChoice, computerChoice){
+   let userChoice = enterChoice.toLowerCase()
+   
+   return userChoice
+    }
+    
+     function playRound(humanChoice, computerChoice){
    if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper"){
      humanScore++
-     console.log(`
-Human Choice : ${humanChoice}
-Computer Choice : ${computerChoice}
-You win! ${humanChoice} beat ${computerChoice}
-Human Score: ${humanScore}
+     displayResult.innerHTML = `
+Human Choice : ${humanChoice}<br>
+Computer Choice : ${computerChoice}<br>
+You win! ${humanChoice} beat ${computerChoice}<br>
+Human Score: ${humanScore}<br>
 Computer Score : ${computerScore}
-`)
+`
    }
    else if(computerChoice === "rock" && humanChoice === "scissors" || computerChoice === "paper" && humanChoice === "rock" || computerChoice === "scissors" && humanChoice === "paper"){
      computerScore++
-     console.log(`
-Human Choice : ${humanChoice}
-Computer Choice : ${computerChoice}
-You lose! ${computerChoice} beat ${humanChoice}
-Human Score: ${humanScore}
+     displayResult.innerHTML= `
+Human Choice : ${humanChoice}<br>
+Computer Choice : ${computerChoice}<br>
+You lose! ${computerChoice} beat ${humanChoice}<br>
+Human Score: ${humanScore}<br>
 Computer Score : ${computerScore}
-`)
+`
    }
    else if(humanChoice === computerChoice){
-     console.log(`
-Human Choice : ${humanChoice}
-Computer Choice : ${computerChoice}
-- Draw -
-Human Score: ${humanScore}
+    displayResult.innerHTML = `
+Human Choice : ${humanChoice}<br>
+Computer Choice : ${computerChoice}<br>
+- Draw - <br>
+Human Score: ${humanScore}<br>
 Computer Score : ${computerScore}
-`)
+`
    }
  }
  let computerSelection = getComputerChoice();
@@ -68,27 +57,5 @@ Computer Score : ${computerScore}
  
  
  playRound(humanSelection, computerSelection)
-
- if(!process && i > 1){
-if(humanScore > computerScore){console.log("Human Win - Incomplete tournament")}
-else if(humanScore < computerScore){
-  console.log("Computer Win - Incomplete tournament")}
-else if(humanScore === computerScore){
-  console.log("Draw Game - Incomplete tournament")}
-  break;
- }
- else if(!process){
-  console.log("Game cancelled")
-  break;
- }
- else if(i === 5){
-   if(humanScore > computerScore){
-  console.log("Human Win")}
-else if(humanScore < computerScore){
-  console.log("Computer Win")}
-else if(humanScore === computerScore){
-  console.log("Draw Game")}
-  }
- i++
- }
-}
+  })
+})
